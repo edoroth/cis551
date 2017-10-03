890608e317f96aa81813895be5b45826cd1074d6
Edo Roth <edoroth@spec08.seas.upenn.edu>
2017-10-02 20:45:47 -0400

    Instructions for running the assignment

---
+++ part2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+//Modified code to remove security issues
+//Edo Roth (edoroth)
+
+
+char msg[1024];
+
+int match(char *s1, char *s2) {
+  while( *s1 != '\0' && *s2 != 0 && *s1 == *s2 ){
+    fflush(stdout);
+    s1++; s2++;
+  }
+  return( *s1 - *s2 );
+}
+
+void welcome(char *str) { puts(str); }
+void goodbye(char *str) { void exit(); puts(str); exit(1); }
+
+int main(){
+   char *good = "Message accepted, my dear friend!\n";
+   char *evil = "Invalid password, get out of here!\n";
+   char name[16], pw[16]; /* 16 characters for username & password */
+
+   printf("Enter a message (1024 characters): ");
+   fgets(msg, 1024, stdin);
+
+   printf("User: "); fgets(name, 16, stdin);
+   printf("Password: "); fgets(pw, 16, stdin);
+   if( match(name,pw) == 0 )
+      welcome(good);
+   else
+      goodbye(evil);
+   printf("Your message is: %s\n", msg);
+}
+
+
